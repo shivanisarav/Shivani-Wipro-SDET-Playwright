b9fa8057a6cb7c43f78955354312c6697c2eabb0
shivanisarav <shivanicloudworks@gmail.com>
2026-04-30 17:23:38 +0530

    Add multiple JavaScript solutions to solutions.js
    This file contains multiple JavaScript solutions for various problems, including a swapper, type checker, initial creator, cleaner, grocery list manipulator, and value finder.

---
+++ solutions.js
@@ -0,0 +1,63 @@
+// Solution 1 (Swapper Question)
+
+let a = 5;
+let b = 10;
+
+[a,b] =[b,a]
+console.log("a : "+a);
+console.log("b : "+b);
+//==============================================================================================================================================
+
+// Solution 2 (Type Checker)
+
+function checkType(input){
+    if (typeof input === "string"){
+        console.log("This is a String");
+
+    }else if (typeof input === "number"){
+        console.log("This is a number");
+    }
+    else{
+        console.log("Please provide valid Data type");
+    }
+}
+checkType(11);
+checkType("Prabhav Tiwari");
+
+//==============================================================================================================================================
+
+// Solution 3 (Initial Creator)
+
+let fullName = "John Doe";
+let initialLetter = fullName.split(" ")[0][0] + fullName.split(" ")[1][0];
+console.log(initialLetter);
+
+//==============================================================================================================================================
+
+// Solution 4 (The Cleaner)
+
+let messy = " Javascript is fun ";
+let cleanedSentence = messy.trim();
+let LowerCaseSentence= cleanedSentence.toLowerCase();
+console.log(LowerCaseSentence);
+
+//===============================================================================================================================================
+
+// Solution 5 (The Grocery list)
+
+let cart = ["Bread", "Curd", "Cheese"]
+cart.push("Milk");
+cart.unshift("Eggs");
+cart.pop();
+console.log(cart);
+
+//================================================================================================================================================
+
+// Solution 6 (Value Finder)
+
+let numbers = [10,20,30,40,50];
+let numberExists = numbers.includes(30);
+let numberAtIndex = numbers.indexOf(50);
+console.log(numberExists);
+console.log(numberAtIndex);
+
