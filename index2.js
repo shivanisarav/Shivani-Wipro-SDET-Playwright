60ea29d0419cebc4a38ac17126aa0ba45224ea3d
shivanisarav <shivanicloudworks@gmail.com>
2026-04-30 15:52:38 +0530

    if else , loops , arrays

---
+++ index2.js
@@ -0,0 +1,33 @@
+console.log("Outside block, l:", l); 
+console.log("Outside block, v:", v); 
+console.log("Outside block, v:", v); 
+
+let arr = [1, "Aaryan", 3, true, 5];
+let arr2 = [1, 2, 3, 4, 5];
+
+
+
+
+let i = 0;
+let j = 10;
+
+if(i > j) {
+    console.log("i is greater");
+} else {
+    console.log("j is greater");
+}
+
+
+
+let choice = "a";
+
+switch(choice) {
+    case "A":
+        console.log("Choice is A");
+        break;
+    case "B":
+        console.log("Choice is B");
+        break;  
+    default:
+        console.log("Invalid choice");
+}
