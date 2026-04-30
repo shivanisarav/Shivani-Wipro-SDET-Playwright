ff4a1c631bd10b86efc4b709da903c56861767b9
shivani M S <shivani200076@gmail.com>
2026-04-30 12:31:32 +0530

    Added JS logical and mathematical operations

---
+++ index.js
@@ -0,0 +1,28 @@
+let str = "Hello, World!";
+
+//string operations
+console.log("String:", str);
+console.log("Length of string:", str.length);
+console.log("Uppercase:", str.toUpperCase());
+console.log("Lowercase:", str.toLowerCase());
+console.log("Substring (0-5):", str.substring(0, 5));
+
+//mathematical operations
+let a = 10;
+let b = 3;
+
+console.log("Addition:", a + b); 
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Remainder:", a % b);
+
+//logical operations
+console.log("a > b:", a > b); // false
+console.log("a < b:", a < b); // true
+console.log("a == b:", a == b); // false (value check)
+console.log("a === b:", a === b); // false (value + type check)
+console.log("a != b:", a != b); // true
+console.log("a !== b:", a !== b); // true
+console.log("a >= b:", a >= b); // false
+console.log("a <= b:", a <= b); //true 
